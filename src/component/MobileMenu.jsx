58b1be0c10e009543376f257c2a8029f12bcffbf
Raunak Ray <raunakay3260@gmail.com>
2025-08-31 09:14:24 +0530

    Fix bugs for mobile devices

--- src/component/MobileMenu.jsx
+++ src/component/MobileMenu.jsx
@@ -1,31 +1,52 @@
 import React from 'react'
 
-const MobileMenu = ({menuOpen, setMenuOpen}) => {
+const MobileMenu = ({ menuOpen, setMenuOpen }) => {
   return (
-    <div className={`top-0 fixed left-0 w-full bg-[rgba(10, 10, 10, 0.8)] z-40 flex flex-col justify-center items-center transition-all duration-300 ease-in-out 
-    ${menuOpen ? "h-screen opacity-100 pointer-events-auto" : "h-screen opacity-0 pointer-events-none"}
-    `}>
-    <button onClick={() => setMenuOpen(false)} 
-            className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer"
-            aria-label='close menu'  >
+    <div
+      className={`fixed top-0 left-0 w-full h-screen bg-[rgba(10,10,10,0.6)] backdrop-blur-md z-40 flex flex-col justify-center items-center transition-all duration-300 ease-in-out 
+      ${menuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}
+    `}
+    >
+      <button
+        onClick={() => setMenuOpen(false)}
+        className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer"
+        aria-label="close menu"
+      >
         &times;
-    </button>
-    <a href="#home" onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-white transform transition-transform duration-300
-    ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}>
-            Home
-    </a>
-    <a href="#about" onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-white transform transition-transform duration-300
-    ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}>
+      </button>
+
+      <a
+        href="#home"
+        onClick={() => setMenuOpen(false)}
+        className={`text-2xl font-semibold text-white transform transition-all duration-300
+        ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}
+      >
+        Home
+      </a>
+      <a
+        href="#about"
+        onClick={() => setMenuOpen(false)}
+        className={`text-2xl font-semibold text-white transform transition-all duration-300
+        ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}
+      >
         About
-    </a>
-    <a href="#projects" onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-white transform transition-transform duration-300
-    ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}>
+      </a>
+      <a
+        href="#projects"
+        onClick={() => setMenuOpen(false)}
+        className={`text-2xl font-semibold text-white transform transition-all duration-300
+        ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}
+      >
         Projects
-    </a>
-    <a href="#contact" onClick={() => setMenuOpen(false)} className={`text-2xl font-semibold text-white transform transition-transform duration-300
-    ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}>
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setMenuOpen(false)}
+        className={`text-2xl font-semibold text-white transform transition-all duration-300
+        ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}`}
+      >
         Contact
-    </a>
+      </a>
     </div>
   )
 }
